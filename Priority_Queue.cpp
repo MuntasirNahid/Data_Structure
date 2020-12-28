840989ee4873093496ecc702cbc403ff5ba83821
Muntasir2019331119 <69505101+Muntasir2019331119@users.noreply.github.com>
2020-12-28 19:27:21 +0600

    Add files via upload

---
+++ Priority_Queue.cpp
@@ -0,0 +1,38 @@
+//Normally sorted thake descending order a..
+#include<bits/stdc++.h>
+using namespace std;
+void printprq(priority_queue<int>p)
+{
+	while (!p.empty())
+	{
+		cout << p.top() << " ";
+		p.pop();
+	}
+	//or iterator use korey kora jay...
+}
+int main()
+{
+	priority_queue<int>pq;
+	//ascending order er jonno
+	//priority_queue<int,vector<int>,greater<int> >pq;
+	pq.push(-5);
+	pq.push(-10);
+	pq.push(-2);
+	pq.push(-7);
+	pq.push(-5);
+
+	cout << "the max element = " << pq.top() << endl;
+	pq.pop();
+	cout << "the max element = " << pq.top() << endl;
+	pq.pop();
+	cout << "the max element = " << pq.top() << endl;
+	
+	pq.push(100);
+	pq.push(756);
+	pq.push(32);
+	pq.push(-90);
+	cout << "The Element of the priority_queue :" << endl;
+	printprq(pq);
+
+
+}
